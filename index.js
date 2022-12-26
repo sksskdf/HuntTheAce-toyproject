74712f5d7977ab1c1f7a528f7e7c26f034116236
hyununam <sksskdfg@gmail.com>
2022-12-26 22:11:26 +0900

    2022-12-26

--- index.js
+++ index.js
@@ -12,15 +12,46 @@ const cardGridMap = {
   4: ".card-pos-d",
 };
 
+const aceId = 4;
+
+let cards = [];
+
 const cardBackImgPath = "/images/card-back-blue.png";
 
 const cardContainerElem = document.querySelector(".card-container");
 
-const createCards = () => {
-  cardObjectDefinitions.forEach((cardItem) => createCard(cardItem));
-};
+const collapsedGridAreaTemplate = '"a a" "a a"';
+const cardCollectionCellClass = ".card-pos-a";
 
-const createCard = (cardItem) => {
+const numCards = cardObjectDefinitions.length;
+
+let cardPositions = [];
+
+let gameInProgress = false;
+let shufflingInProgress = false;
+let cardsRevealed = false;
+
+const currentGameStatusElem = document.querySelector(".current-status");
+const scoreContainerElem = document.querySelector(".header-score-container");
+const scoreElem = document.querySelector(".score");
+const roundContainerElem = document.querySelector(".header-round-container");
+const roundElem = document.querySelector(".round");
+
+const winColor = "greent";
+const loseColor = "red";
+const primaryColor = "black";
+
+let roundNum = 0;
+let maxRounds = 4;
+let score = 0;
+
+loadGame();
+
+function createCards() {
+  cardObjectDefinitions.forEach((cardItem) => createCard(cardItem));
+}
+
+function createCard(cardItem) {
   const cardElem = createElement("div");
   const cardInnerElem = createElement("div");
   const cardFrontElem = createElement("div");
@@ -57,36 +88,287 @@ const createCard = (cardItem) => {
   addChildElement(cardElem, cardInnerElem);
 
   addCardToGridCell(cardElem);
-};
 
-const createElement = (elemType) => {
+  initializeCardPositions(cardElem);
+
+  attatchClickEventHandlerToCard(cardElem);
+}
+
+function createElement(elemType) {
   return document.createElement(elemType);
-};
+}
 
-const addClassToElement = (elem, className) => {
+function addClassToElement(elem, className) {
   elem.classList.add(className);
-};
+}
 
-const addIdToElement = (elem, id) => {
+function addIdToElement(elem, id) {
   elem.id = id;
-};
+}
 
-const addSrcToImageElem = (imgElem, src) => {
+function addSrcToImageElem(imgElem, src) {
   imgElem.src = src;
-};
+}
 
-const addChildElement = (parentElem, childElem) => {
+function addChildElement(parentElem, childElem) {
   parentElem.appendChild(childElem);
-};
+}
 
-const addCardToGridCell = (card) => {
+function addCardToGridCell(card) {
   const cardPositionClassName = mapCardIdToGridCell(card);
   const cardPosElem = document.querySelector(cardPositionClassName);
   addChildElement(cardPosElem, card);
-};
+}
 
-const mapCardIdToGridCell = (card) => {
+function mapCardIdToGridCell(card) {
   return cardGridMap[card.id];
-};
+}
 
-createCards();
+function startGame() {
+  initializeNewGame();
+  startRound();
+}
+
+function initializeNewGame() {
+  score = 0;
+  roundNum = 0;
+
+  shufflingInProgress = false;
+  updateStatusElement(scoreContainerElem, "flex");
+  updateStatusElement(roundContainerElem, "flex");
+  updateStatusElement(
+    scoreElem,
+    "block",
+    primaryColor,
+    `Score <span class='badge'>${score}</span>`
+  );
+  updateStatusElement(
+    roundElem,
+    "block",
+    primaryColor,
+    `Round <span class='badge'>${roundNum}</span>`
+  );
+}
+
+function chooseCard(card) {
+  if (canChooseCard()) {
+    evaluateCardChoice(card);
+    flipCard(card, false);
+
+    setTimeout(() => {
+      flipCards(false);
+      updateStatusElement(
+        currentGameStatusElem,
+        "block",
+        primaryColor,
+        "Card positions revealed"
+      );
+    }, 3000);
+    cardsRevealed = true;
+  }
+}
+
+function attatchClickEventHandlerToCard(card) {
+  card.addEventListener("click", () => chooseCard(card));
+}
+
+function updateStatusElement(elem, display, color, innerHTML) {
+  elem.style.display = display;
+
+  if (arguments.length > 2) {
+    elem.style.color = color;
+    elem.innerHTML = innerHTML;
+  }
+}
+
+function outputChoiceFeedBack(hit) {
+  if (hit) {
+    updateStatusElement(
+      currentGameStatusElem,
+      "block",
+      winColor,
+      "Hit!! - Well Done!! :)"
+    );
+  } else {
+    updateStatusElement(
+      currentGameStatusElem,
+      "block",
+      loseColor,
+      "Missed!! :("
+    );
+  }
+}
+
+function calculateScoreToAdd(roundNum) {
+  switch (roundNum) {
+    case 1:
+      return 100;
+    case 2:
+      return 50;
+    case 3:
+      return 25;
+    default:
+      return 10;
+  }
+}
+
+function calculateScore() {
+  const scoreToAdd = calculateScoreToAdd(roundNum);
+  score = score + scoreToAdd;
+}
+
+function updateScore() {
+  calculateScore();
+}
+
+function evaluateCardChoice(card) {
+  if (card.id == aceId) {
+    updateScore();
+    outputChoiceFeedBack(true);
+  } else {
+    outputChoiceFeedBack(false);
+  }
+}
+
+function canChooseCard() {
+  return gameInProgress == true && !shufflingInProgress && !cardsRevealed;
+}
+
+function startRound() {
+  initializeNewRound();
+  collectCards();
+  flipCards(true);
+  shuffleCards();
+}
+
+function shuffleCards() {
+  const id = setInterval(shuffle, 12);
+  let shuffleCount = 0;
+
+  function shuffle() {
+    randomizeCardPositions();
+
+    if (shuffleCount == 500) {
+      clearInterval(id);
+      dealCards();
+    } else {
+      shuffleCount++;
+    }
+  }
+}
+
+function dealCards() {
+  addCardsToAppropriateCell();
+  const areasTemplate = returnGridAreasMappedToCardPos();
+  transformGridArea(areasTemplate);
+}
+
+function returnGridAreasMappedToCardPos() {
+  let firstPart = "";
+  let secondPart = "";
+  let areas = "";
+
+  cards.forEach((card, index) => {
+    if (cardPositions[index] == 1) {
+      areas = areas + "a ";
+    } else if (cardPositions[index] == 2) {
+      areas = areas + "b ";
+    } else if (cardPositions[index] == 3) {
+      areas = areas + "c ";
+    } else if (cardPositions[index] == 4) {
+      areas = areas + "d ";
+    }
+
+    if (index == 1) {
+      firstPart = areas.substring(0, areas.length - 1);
+      areas = "";
+    } else if (index == 3) {
+      secondPart = areas.substring(0, areas.length - 1);
+    }
+  });
+  return `"${firstPart}" "${secondPart}"`;
+}
+
+function addCardsToAppropriateCell() {
+  cards.forEach((card) => {
+    addCardToGridCell(card);
+  });
+}
+
+function randomizeCardPositions() {
+  const random1 = Math.floor(Math.random() * numCards) + 1;
+  const random2 = Math.floor(Math.random() * numCards) + 1;
+
+  const temp = cardPositions[random1 - 1];
+
+  cardPositions[random1 - 1] = cardPositions[random2 - 1];
+  cardPositions[random2 - 1] = temp;
+}
+
+function initializeNewRound() {
+  roundNum++;
+  playGame.disabled = true;
+
+  gameInProgress = true;
+  shufflingInProgress = true;
+  cardsRevealed = false;
+
+  updateStatusElement(
+    currentGameStatusElem,
+    "block",
+    primaryColor,
+    "Shuffling..."
+  );
+  updateStatusElement(
+    roundElem,
+    "block",
+    primaryColor,
+    `Round <span class='badge'>${roundNum}</span>`
+  );
+}
+
+function initializeCardPositions(card) {
+  cardPositions.push(card.id);
+}
+
+function collectCards() {
+  transformGridArea(collapsedGridAreaTemplate);
+  addCardsToGridAreaCell(cardCollectionCellClass);
+}
+
+function transformGridArea(areas) {
+  cardContainerElem.getElementsByClassName.gridTemplateAreas = areas;
+}
+
+function addCardsToGridAreaCell(cellPositionClassName) {
+  const cellPositionElem = document.querySelector(cellPositionClassName);
+
+  cards.forEach((card, index) => {
+    addChildElement(cellPositionElem, card);
+  });
+}
+
+function loadGame() {
+  createCards();
+
+  cards = document.querySelectorAll(".card");
+
+  playGame.addEventListener("click", () => startGame());
+}
+
+function flipCard(card, flipToBack) {
+  const innerCardElem = card.firstChild;
+  if (flipToBack && !innerCardElem.classList.contains("flip-it")) {
+    innerCardElem.classList.add("flip-it");
+  } else if (innerCardElem.classList.contains("flip-it")) {
+    innerCardElem.classList.remove("flip-it");
+  }
+}
+
+function flipCards(flipToBack) {
+  cards.forEach((card, index) => {
+    setTimeout(() => {
+      flipCard(card, flipToBack);
+    }, index * 100);
+  });
+}
